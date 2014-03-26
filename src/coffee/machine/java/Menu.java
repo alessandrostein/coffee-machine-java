7172163c3d624681395e8f2d8e425d3cdbc3bb20
alessandrostein <ale182juve@msn.com>
2014-03-25 23:35:22 -0300

    Partial commit, menu and product

---
+++ src/coffee/machine/java/Menu.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package coffee.machine.java;
+
+/**
+ *
+ * @author alessandrostein
+ */
+public class Menu {
+    
+    public void cafe(){};
+   
+    public void cafeAcucar(){};
+    
+    public void cafeCreme(){};
+    
+    public void CafeCremeAcucar(){};
+}
